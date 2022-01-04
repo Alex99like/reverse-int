c5493aed9d5aff73e688a5f7f61fb807416d45f3
Aleksander <aleksievisa@gmail.com>
2022-01-04 17:55:42 +0300

    update: index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        let num = String(n).split('');
+        num.shift();
+        return num.reverse().join('');
+      }
+      let num = Number(String(n).split('').reverse().join(''));
+      return num
 }
